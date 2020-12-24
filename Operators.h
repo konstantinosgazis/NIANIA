d12422034d6c9df65fd87a9df188d9fe02cd2a99
DESKTOP-K7HFQLG\nmarm <n.marmaras@hotmail.com>
2020-12-24 20:11:39 +0200

    Operator overloading arithmetic, boolean and comparing complete.

--- Operators.h
+++ Operators.h
@@ -1,26 +1,169 @@
 #include "JSONlang.h"
-bool operator== (Variable left , Variable right)    {return -1;}
-bool operator!= (Variable left , Variable right)    {return -1;}
-bool operator<  (Variable left , Variable right)    {return -1;}
-bool operator>  (Variable left , Variable right)    {return -1;}
-bool operator<= (Variable left , Variable right)    {return -1;}
-bool operator>= (Variable left , Variable right)    {return -1;}
-// Variable operator+  (Variable left , Variable right)    {return;}
-// Variable operator-  (Variable left , Variable right)    {return;}
-// Variable operator*  (Variable left , Variable right)    {return;}
-// Variable operator/  (Variable left , Variable right)    {return;}
-// Variable operator%  (Variable left , Variable right)    {return;}
-bool operator== (Variable left , int  right)    {return -1;}
-bool operator!= (Variable left , int  right)    {return -1;}
-bool operator<  (Variable left , int  right)    {return -1;}
-bool operator>  (Variable left , int  right)    {return -1;}
-bool operator<= (Variable left , int  right)    {return -1;}
-bool operator>= (Variable left , int  right)    {return -1;}
-// Variable operator+  (Variable left , int  right)    {return;}
-// Variable operator-  (Variable left , int  right)    {return;}
-// Variable operator*  (Variable left , int  right)    {return;}
-// Variable operator/  (Variable left , int  right)    {return;}
-// Variable operator%  (Variable left , int  right)    {return;}
+
+bool operator== (Variable left , Variable right)    
+{
+    return -1;
+}
+bool operator!= (Variable left , Variable right)    
+{
+    return -1;
+}
+
+Variable operator&& (Variable left, Variable right)
+{
+    if(left.getType() != BOOLEAN || right.getType() != BOOLEAN)
+    {
+        cout << "Task failed succesfully! Must AND only booleans!" << endl;
+        exit(0);
+    }
+    return *(new Boolean(left.getBool() && right.getBool()));
+}
+
+Variable operator|| (Variable left, Variable right)
+{
+    if(left.getType() != BOOLEAN || right.getType() != BOOLEAN)
+    {
+        cout << "Task failed succesfully! Must OR only booleans!" << endl;
+        exit(0);
+    }
+    return *(new Boolean(left.getBool() || right.getBool()));
+}
+
+
+Variable operator<  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must compare (<) only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() < left.getNumber()));
+}
+Variable operator>  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must compare (>) only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() > left.getNumber()));
+}
+Variable operator<= (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must compare (<=) only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() <= left.getNumber()));
+}
+Variable operator>= (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must compare (>=) only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() >= left.getNumber()));
+}
+
+Variable operator+  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must add only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() + left.getNumber()));
+}
+
+Variable operator-  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must subtract only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() - left.getNumber()));
+}
+
+Variable operator*  (Variable left , Variable right)    
+{
+   if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must multiply only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() * left.getNumber()));
+}
+
+Variable operator/  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must div only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(right.getNumber() / left.getNumber()));
+}
+
+Variable operator%  (Variable left , Variable right)    
+{
+    if(left.getType() != NUMBER || right.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must module only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(static_cast<int>(right.getNumber()) % static_cast<int>(left.getNumber())));
+}
+
+
+Variable operator+  (Variable left , int  right)    
+{
+    if(left.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must add only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(left.getNumber())) + right;
+}
+Variable operator-  (Variable left , int  right)    
+{
+    if(left.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must subtract only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(left.getNumber())) - right;
+}
+Variable operator*  (Variable left , int  right)    
+{
+    if(left.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must multiply only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(left.getNumber())) * right;
+}
+Variable operator/  (Variable left , int  right)    
+{
+    if(left.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must div only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(left.getNumber())) / right;
+}
+Variable operator%  (Variable left , int  right)    
+{
+    if(left.getType() != NUMBER)
+    {
+        cout << "Task failed succesfully! Must subtract only numbers!" << endl;
+        exit(0);
+    }
+    return *(new Number(static_cast<int>(left.getNumber()))) % right;
+}
+
 vector<Variable>* operator,(Variable a, Variable b){
     vector<Variable>* v = new vector<Variable>();
     v->push_back(a);
--- main.cpp
+++ main.cpp
@@ -3,23 +3,27 @@
 PROGRAM_BEGIN
 
 JSON(emp) = STRING("Hello, world!")
-JSON(num) = NUMBER(23.5)
+JSON(num1) = NUMBER(23.5)
+JSON(num2) = NUMBER(16.5)
 JSON(bol) = BOOLEAN(TRUE)
-JSON(ar) = ARRAY [emp, num , STRING("mesaa")]
+JSON(ar) = ARRAY [emp, num1 , STRING("mesaa")]
 
-JSON(t) = ARRAY [ ar , ARRAY [ num ] , ARRAY [ ar , ar]]
+JSON(t) = ARRAY [ ar , ARRAY [ num1 ] , ARRAY [ ar , ar]]
 
 JSON(week_temperatures) = ARRAY [
 NUMBER(20),NUMBER(19.5),NUMBER(19),NUMBER(20),
 NUMBER(19),NUMBER(18.5),NUMBER(19)
 ]
 
+JSON(sum) = num1 && bol
+
 SET week_temperatures[2] ASSIGN NUMBER(22)
 
 PRINT(emp)
-PRINT(num)
+PRINT(num1)
 PRINT(ar)
 PRINT(bol)
+PRINT(sum)
 
 PRINT(t)
 PRINT(week_temperatures)
